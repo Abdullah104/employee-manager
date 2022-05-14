eaaf6a7531aaf4d5da5d35e912c112a4830abf4d
Abdullah Sharef <38993855+Abdullah104@users.noreply.github.com>
2022-05-14 22:51:27 +0300

    Employee view route implementation.

--- src/components/ViewEmployee.vue
+++ src/components/ViewEmployee.vue
@@ -1,7 +1,95 @@
-<template><div id="view-employee"><h3>View Employee</h3></div></template>
+<template>
+  <div id="view-employee">
+    <ul class="collection with-header">
+      <li class="collection-header">
+        <h4>{{ name }}</h4>
+      </li>
+      <li class="collection-item">Employee Id#: {{ employeeId }}</li>
+      <li class="collection-item">Department: {{ department }}</li>
+      <li class="collection-item">Position: {{ position }}</li>
+    </ul>
+    <router-link class="btn grey" to="/">Back</router-link>
+    <button class="btn red" @click="deleteEmployee">Delete</button>
+  </div>
+</template>
 
 <script>
+import firestore from "../firebase_init";
+import {
+  collection,
+  getDocs,
+  where,
+  query,
+  limit,
+  deleteDoc,
+} from "firebase/firestore";
+
 export default {
   name: "view-employee",
+  data() {
+    return {
+      employeeId: null,
+      name: null,
+      department: null,
+      position: null,
+      documentReference: null,
+    };
+  },
+  async beforeRouteEnter(to, _, next) {
+    if (to.params.employeeId != null) {
+      const employeesCollection = collection(firestore, "employees");
+
+      const employeesQuery = query(
+        employeesCollection,
+        where("employee_id", "==", to.params.employeeId),
+        limit(1)
+      );
+
+      const snapshot = await getDocs(employeesQuery);
+      const document = snapshot.docs[0];
+      const { name, department, position } = document.data();
+
+      next((vm) => {
+        vm.employeeId = to.params.employeeId;
+        vm.name = name;
+        vm.department = department;
+        vm.position = position;
+        vm.documentReference = document.ref;
+      });
+    }
+  },
+  watch: {
+    $route: "fetchData",
+  },
+  methods: {
+    async fetchData() {
+      if (this.$route.params.employeeId != null) {
+        const employeesCollection = collection(firestore, "employees");
+
+        const employeesQuery = query(
+          employeesCollection,
+          where("employee_id", "==", this.$route.params.employeeId),
+          limit(1)
+        );
+
+        const snapshot = await getDocs(employeesQuery);
+        const document = snapshot.docs[0];
+        const { name, department, position } = document.data();
+
+        this.employeeId = to.params.employeeId;
+        this.name = name;
+        this.department = department;
+        this.position = position;
+        this.documentReference = document.ref;
+      }
+    },
+    async deleteEmployee() {
+      if (confirm("Are you Sure?")) {
+        await deleteDoc(this.documentReference);
+
+        this.$router.push("/");
+      }
+    },
+  },
 };
 </script>
